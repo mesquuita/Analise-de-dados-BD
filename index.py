6e7162be43bac9746fa5e75661ce6bd88e0ab065
mesquuita <mesquitagabrieldev@gmail.com>
2026-04-06 10:28:19 -0300

    Graficos e complementos de analise

--- index.py
+++ index.py
@@ -32,3 +32,52 @@ col2.metric("Velocidade Média", round(df_filtered["avg_speed_kmh"].mean(), 2))
 col3.metric("Atraso Médio Ônibus", round(df_filtered["bus_delay_minutes"].mean(), 2))
 
 
+# Gráfico por região
+
+region = df_filtered.groupby("region")["vehicle_count"].mean().reset_index()
+region = region.sort_values("vehicle_count", ascending=False)
+
+fig1 = px.bar(
+    region,
+    x="region",
+    y="vehicle_count",
+    title="Fluxo por Região",
+    text=region["vehicle_count"].round(2)
+)
+
+fig1.update_traces(textposition='outside')
+
+fig1.update_layout(
+    yaxis=dict(range=[
+        region["vehicle_count"].min() - 0.5,
+        region["vehicle_count"].max() + 0.5
+    ])
+)
+
+st.plotly_chart(fig1, width='stretch')
+
+
+# Gráfico por hora
+
+hour = df_filtered.groupby("hour")["vehicle_count"].mean().reset_index()
+
+fig2 = px.line(hour, x="hour", y="vehicle_count", title="Fluxo por Hora")
+st.plotly_chart(fig2, use_container_width=True)
+
+
+# Impacto da chuva
+
+fig3 = px.box(df_filtered, x="rain", y="avg_speed_kmh", title="Impacto da Chuva na Velocidade")
+st.plotly_chart(fig3, use_container_width=True)
+
+
+#Eventos sociais
+
+fig4 = px.box(df_filtered, x="social_event", y="vehicle_count", title="Impacto de Eventos no Trânsito")
+st.plotly_chart(fig4, use_container_width=True)
+
+
+# Correlação
+corr = df_filtered[["vehicle_count", "avg_speed_kmh", "bus_delay_minutes"]].corr()
+fig5 = px.imshow(corr, text_auto=True, title="Correlação")
+st.plotly_chart(fig5, use_container_width=True)
